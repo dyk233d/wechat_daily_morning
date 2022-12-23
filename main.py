2b5928ac5df6c47583460d5fd9c2d86c39919108
dyk233d <56498732+dyk233d@users.noreply.github.com>
2022-12-23 09:56:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 call_back_id = os.environ["CALL_BACK_ID"]
 template_id = os.environ["TEMPLATE_ID"]
+tianxin_apikey = os.environ["APIKEY"]
 
 
 def get_weather():
@@ -39,10 +40,12 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
+  #words = requests.get("https://api.shadiao.pro/chp")   #这个能用
+  param = {'key': tianxin_apikey}
+  words = requests.post("https://apis.tianapi.com/healthtip/index",params = param)
   if words.status_code != 200:
     return get_words()
-  return words.json()['data']['text']
+  return words.json()['result']['content']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
@@ -53,7 +56,7 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea = get_weather()
 data = {"date":{"value":str(today)[:10], "color":get_random_color()},"city":{"value":city, "color":get_random_color()},"weather":{"value":wea},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+#res = wm.send_template(user_id, template_id, data)
+#print(res)
 res = wm.send_template(call_back_id, template_id, data)
 print(res)
